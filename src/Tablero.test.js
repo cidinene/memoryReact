3213cdb0b1a38113a53bf279bd121294a27535f9
cidinene <cidinene@users.noreply.github.com>
2025-03-19 16:51:22 +0000

    deploy: ec1b689a5358ca3296ccc1ec40b536635300a43a

--- src/Tablero.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Tablero from './Tablero';
-
-test('renders Card', () => {
-  render(<Tablero />);
-  const numeroCartas = screen.getByText(/8/i);
-  expect(numeroCartas).toBeInTheDocument();
-});
