b19e8fb608346c69011d3bd5ecda0a57b14d62a8
cidinene <cidinene@users.noreply.github.com>
2025-03-19 18:37:58 +0000

    deploy: 95391ef413c5bf4fac56553330fc230f1c97ca82

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ const BASE_URL = '/memoryReact';  // Añade la subruta aquí
 const urlsToCache = [
   `${BASE_URL}/`,
   `${BASE_URL}/index.html`,
-  `${BASE_URL}/static/js/main.chunk.js`,
-  `${BASE_URL}/static/js/2.chunk.js`,
-  `${BASE_URL}/static/js/bundle.js`,
-  `${BASE_URL}/static/css/main.chunk.css`,
+  `${BASE_URL}/static/js/main.9c701b54.js`,
+  `${BASE_URL}/static/js/main.9c701b54.js.LICENSE.txt`,
+  `${BASE_URL}/static/js/main.9c701b54.js.map`,
+  `${BASE_URL}/static/css/main.df6056e8.css`,
   `${BASE_URL}/manifest.json`,
   `${BASE_URL}/icon-192x192.png`,
   `${BASE_URL}/icon-512x512.png`
